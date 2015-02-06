8d789c85189faa53ce30154adca3913d585882d2
Gautham Maran <gauthamcity12@gmail.com>
2015-02-06 00:07:34 -0500

    Added Username + Hash Transmission

--- Programming Assignment 1/src/TCPServer.java
+++ Programming Assignment 1/src/TCPServer.java
@@ -1,12 +1,27 @@
 import java.net.*;
 import java.util.HashMap;
+import java.util.Random;
 import java.io.*;
 
 public class TCPServer {
 	
 	private static HashMap<String, String> map = new HashMap<String, String>();
-	private static final int BUFSIZE = 32;
+	private static final int BUFSIZE = 64;
 	
+	public static String nonceCreation(){
+		Random ran = new Random();
+		int size = 63;
+		char data = ' ';
+		String nonce = "";
+
+		for (int i=0; i<=size; i++) {
+		  data = (char)(ran.nextInt(25)+97);
+		  nonce = data + nonce;
+		}
+
+		System.out.println("Nonce:" + nonce);
+		return nonce;
+	}
 	
 	public static void main(String[] args) throws IOException{
 		if(args.length != 1){
@@ -34,23 +49,36 @@ public class TCPServer {
 			
 			InputStream in = clientSocket.getInputStream();
 			OutputStream out = clientSocket.getOutputStream();
-			
-//			while((recvMessageSize = in.read(byteBuffer))!= -1){
-//				out.write(byteBuffer, 0, recvMessageSize);
-//			}
+
 			String welcome = "Welcome to our service.";
 			
+			// AUTHENTICATION REQUEST RECEIVED HERE
+			in.read(byteBuffer, 0 ,1);
+			String input = new String(byteBuffer);
+			System.out.println("Y:" + input);
+			
+			/// Write out cryptographic nonce
+			String nonce = nonceCreation();
+			out.write(nonce.getBytes());
+			
+			/// Received Username & Hash
 			in.read(byteBuffer);
-			String user = new String(byteBuffer);
-			System.out.println(user);
-			//in.read(byteBuffer);
-			String pass = new String(byteBuffer);
-			System.out.println(pass);
-//			if(user.equals("user") && pass.equals("pass")){
-//				
-//				//out.write(welcome.getBytes());
-//			}
-//			out.write(welcome.getBytes());
+			String userHash = new String(byteBuffer);
+			System.out.println("User & Hash:"+userHash);
+			
+			String user = "";
+			String hash = "";
+			for(int i =0; i < userHash.length(); i++){
+				if(userHash.charAt(i) == '*'){
+					user = userHash.substring(0, i);
+					hash = userHash.substring(i+1);
+				}
+			}
+			System.out.println("User:"+user+"----"+"Hash:"+hash);
+			
+			
+			
+			
 			clientSocket.close();
 		}
 		
